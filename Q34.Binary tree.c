c027612884a75e2c7871641a0be5f9ac2b44f2a9
balajiyd <124142672+balajiyd@users.noreply.github.com>
2023-08-22 14:19:06 +0530

    Create Q34.Binary tree.c

---
+++ Q34.Binary tree.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node *left;
+    struct Node *right;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node *node = (struct Node *)malloc(sizeof(struct Node));
+    node->data = data;
+    node->left = NULL;
+    node->right = NULL;
+    return node;
+}
+
+// Function to perform an in-order traversal of the binary tree
+void inorderTraversal(struct Node *root) {
+    if (root != NULL) {
+        inorderTraversal(root->left);
+        printf("%d ", root->data);
+        inorderTraversal(root->right);
+    }
+}
+
+int main() {
+    // Create the nodes of the binary tree
+    struct Node *root = createNode(1);
+    root->left = createNode(2);
+    root->right = createNode(3);
+    root->left->left = createNode(4);
+    root->left->right = createNode(5);
+    root->right->left = createNode(6);
+    root->right->right = createNode(7);
+
+    printf("In-order traversal of the binary tree: ");
+    inorderTraversal(root);
+    printf("\n");
+
+    return 0;
+}
